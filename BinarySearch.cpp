15a1fdf8a4a45d331959eedef68b6c70f2e9d45e
Om Saini <73247335+omsaini-1441@users.noreply.github.com>
2022-11-24 10:47:15 +0530

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
-using namesapce std;
+using namespace std;
 
-int BinarySearch( int arr, int size, int key){
-	int start =0;
+int BinarySearch( int arr[], int size, int key){
+	int start = 0;
 	int end = size-1;
 	
 	int mid =(start + end)/2;
 	
-	while(start>=end){
+	while(start<=end){
 		if(arr[mid]==key){
 			return mid;
 		}
@@ -25,7 +25,8 @@ int BinarySearch( int arr, int size, int key){
 int main(){
 	int even[5]={2,4,6,8,10};
 	int odd[5]={1,3,5,7,9};
-	
+	 int index = BinarySearch(even,5,6);
+	 cout<<index;
 	
 	return 0;
 }
