4279d1d103a59db892619c89b9bba82babfd41a1
Om Saini <73247335+omsaini-1441@users.noreply.github.com>
2023-03-28 20:15:44 +0530

    Update Reversea a Linked List.cpp

--- Reversea a Linked List.cpp
+++ Reversea a Linked List.cpp
@@ -26,3 +26,16 @@ public:
     }
 };
 
+// recursive way 1
+// call recursion for chotaHead just to find and place head pointer to last element and then coming back from there only and deal with first element later
+// by changing head(first ele)->next->next(choti list ka last ele i.e doosra ele) = head and head->next = NULL;
+Node* reverse1(Node* head){
+    // base case
+    if(head==NULL || head->next== NULL){
+        return head;
+    }
+    Node* ChotaHead = reverse1(head->next);
+        head->next->next=head;
+        head->next =NULL;
+    return chotaHead;
+}
